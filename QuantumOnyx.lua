1c8b09c08e2518f08e58c6fae470f0608c1c21a6
jazzedd <kabra4035@gmail.com>
2025-08-08 17:13:19 +0800

    Update QuantumOnyx.lua

--- QuantumOnyx.lua
+++ QuantumOnyx.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/9sf3eQysjwPEzf60/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jazzedd/iwiw/refs/heads/main/kwwk"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/0v2xhl0St7FmN5r0/raw", true))()
